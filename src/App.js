75690408e441baf944e39ef99899531f907336ff
zubiyabintamostofa <zubiyabinte123@gmail.com>
2022-03-20 12:49:59 +0600

    two

--- src/App.js
+++ src/App.js
@@ -1,41 +1,46 @@
 import logo from './logo.svg';
 import './App.css';
 import { useEffect, useState } from 'react';
+import Countries from './Components/Country/Countries';
 
 function App() {
   return (
     <div className="App">
-      <LoadCountries></LoadCountries>
+      {/* <LoadCountries></LoadCountries> */}
+      <Countries></Countries>
       
     </div>
   )
 }
+// eita ager ta
 
-function LoadCountries(){
-  const [countries,setCountries]=useState([]);
-  useEffect(()=>{
 
-    fetch('https://restcountries.com/v3.1/all')
-    .then(res=>res.json())
-    .then(data=>setCountries(data))
-  },[])
-  return(
-    <div>
-      <h1>visiting Every Country</h1>
-      <h3>Avilable Countries:{countries.length}</h3>
-      {
-        countries.map(country=><Country name={country.name.common} population={country.population}></Country>)
-      }
-    </div>
-  )
-}
-function Country(props){
-  return(
-    <div>
-      <h2>Name:{props.name}</h2>
-      <h4>Population{props.population}</h4>
-    </div>
-  )
-}
+
+// function LoadCountries(){
+//   const [countries,setCountries]=useState([]);
+//   useEffect(()=>{
+
+//     fetch('https://restcountries.com/v3.1/all')
+//     .then(res=>res.json())
+//     .then(data=>setCountries(data))
+//   },[])
+//   return(
+//     <div>
+//       <h1>visiting Every Country</h1>
+//       <h3>Avilable Countries:{countries.length}</h3>
+//       {
+//         countries.map(country=><Country name={country.name.common} population={country.population}></Country>)
+//       }
+//     </div>
+//   )
+// }
+// function Country(props){
+//   return(
+//     <div>
+//       <h2>Name:{props.name}</h2>
+//       <h4>Population{props.population}</h4>
+//     </div>
+//   )
+// }
 
 export default App;
