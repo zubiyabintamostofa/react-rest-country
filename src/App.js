fd59645131f91f59a1c1aa5c279e2878f89a793f
zubiyabintamostofa <zubiyabinte123@gmail.com>
2022-03-20 11:48:02 +0600

    ten

--- src/App.js
+++ src/App.js
@@ -23,6 +23,17 @@ function LoadCountries(){
     <div>
       <h1>visiting Every Country</h1>
       <h3>Avilable Countries:{countries.length}</h3>
+      {
+        countries.map(country=><Country name={country.name.common} population={country.population}></Country>)
+      }
+    </div>
+  )
+}
+function Country(props){
+  return(
+    <div>
+      <h2>Name:{props.name}</h2>
+      <h4>Population{props.population}</h4>
     </div>
   )
 }
